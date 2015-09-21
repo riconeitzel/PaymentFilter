2b0ecdbc0ac4a0e06fba1bee59e468d8863026c5
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2015-09-21 17:53:53 +0200

    add customer attribute to forms to make it accessible in backend

--- app/code/community/RicoNeitzel/PaymentFilter/sql/payfilter_setup/mysql4-upgrade-0.1.6-1.0.0.php
+++ app/code/community/RicoNeitzel/PaymentFilter/sql/payfilter_setup/mysql4-upgrade-0.1.6-1.0.0.php
@@ -14,9 +14,14 @@ $this->addAttribute(
         'backend' => 'payfilter/entity_backend_payment_methods',
         'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_WEBSITE,
         'default' => '',
-        'user_defined' => 0,
+        'user_defined' => 1,
         'required' => 0,
     )
 );
+$attribute = Mage::getSingleton("eav/config")->getAttribute("customer", "allowed_payment_methods");
+
+$attribute->setData("used_in_forms", array('adminhtml_customer'))->setData("is_system", 0);
+$attribute->save();
+
 
 $this->endSetup();
